7af920367ae0099cb381073e7cf15769e8db1831
mohamed samir <mohamad.samir08@gmail.com>
2022-10-03 21:42:51 +0200

    creating new react app & adding filter method + getting data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+		};
+		console.log('constructor');
+	}
+	componentDidMount() {
+		console.log('componentDidMount');
+		fetch('https://reqres.in/api/users')
+			.then((res) => res.json())
+			.then((data) =>
+				this.setState(
+					() => {
+						return { monsters: data.data };
+					},
+					() => console.log(this.state)
+				)
+			);
+	}
+
+	render() {
+		console.log('render');
+		return (
+			<div className="App">
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={(event) => {
+						const searchString = event.target.value.toLocaleLowerCase();
+						const filteredMonsters = this.state.monsters.filter((monster) => {
+							return monster.first_name
+								.toLocaleLowerCase()
+								.includes(searchString);
+						});
+						this.setState(() => {
+							return { monsters: filteredMonsters };
+						});
+					}}
+				/>
+				{this.state.monsters.map((monster) => {
+					return (
+						<div key={monster.id}>
+							<h1>{monster.first_name}</h1>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
